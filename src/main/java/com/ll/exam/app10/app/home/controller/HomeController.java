9c1eed033000c8ff603a273012276a2a11e24943
SG1515 <sunggu507@naver.com>
2022-10-11 13:39:24 +0900

    각각의 액션에서 로그인된 회원과 프로필이미지 얻는 로직 제거, 최대한 시큐리티 세션정보를 활용, 하지만 이미지는 안나오게 됨

--- src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
@@ -1,13 +1,9 @@
 package com.ll.exam.app10.app.home.controller;
 
-import com.ll.exam.app10.app.member.entity.Member;
 import com.ll.exam.app10.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,40 +11,12 @@ public class HomeController {
     private final MemberService memberService;
 
     @GetMapping("/")
-    public String showMain(Principal principal, Model model) {
-        Member loginedMember = null;
-        String loginedMemberProfileImgUrl = null;
-
-        if (principal != null && principal.getName() != null) {
-            loginedMember = memberService.getMemberByUsername(principal.getName());
-        }
-
-        if (loginedMember != null) {
-            loginedMemberProfileImgUrl = loginedMember.getProfileImgUrl();
-        }
-
-        model.addAttribute("loginedMember", loginedMember);
-        model.addAttribute("loginedMemberProfileImgUrl", loginedMemberProfileImgUrl);
-
+    public String showMain() {
         return "home/main";
     }
 
     @GetMapping("/about")
-    public String showAbout(Principal principal, Model model) {
-        Member loginedMember = null;
-        String loginedMemberProfileImgUrl = null;
-
-        if (principal != null && principal.getName() != null) {
-            loginedMember = memberService.getMemberByUsername(principal.getName());
-        }
-
-        if (loginedMember != null) {
-            loginedMemberProfileImgUrl = loginedMember.getProfileImgUrl();
-        }
-
-        model.addAttribute("loginedMember", loginedMember);
-        model.addAttribute("loginedMemberProfileImgUrl", loginedMemberProfileImgUrl);
-
+    public String showAbout() {
         return "home/about";
     }
 }
