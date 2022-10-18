0735595eacc3c4cf972c88c1cedd0157048bede9
SG1515 <sunggu507@naver.com>
2022-10-18 14:44:04 +0900

    글쓰기

--- src/main/java/com/ll/exam/app10/app/security/dto/MemberContext.java
+++ src/main/java/com/ll/exam/app10/app/security/dto/MemberContext.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Getter
--- src/main/java/com/ll/exam/app10/app/security/service/OAuth2UserService.java
+++ src/main/java/com/ll/exam/app10/app/security/service/OAuth2UserService.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -18,9 +19,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 @Transactional(readOnly = true)
